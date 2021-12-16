a5b3d4d64425fe05b537e08d771e986393dbbec1
albert3rd <atpcodeio@gmail.com>
2021-12-16 13:17:54 -1000

    data modeling

--- answers/models.py
+++ answers/models.py
@@ -10,10 +10,13 @@ class Answers(models.Model):
     user_answer = models.TextField()
 
     def answer_return(self, *args, **kwargs):
-        g = geocoder.google(self.user_answer, key=GOOGLE_API_KEY)
+        g = geocoder.mapbox(self.user_answer, key="GOOGLE_API_KEY")
+        self.user_latitude = g.latlng[0]
+        self.user_longitude = g.latlng[1]
 
 
 class Trivia_Questions(models.Model):
+    user_answer = models.ForeignKey(Answers, on_delete=models.CASCADE)
     trivia_question = models.TextField()
     correct_answer = models.TextField()
     correct_longitude = models.FloatField(blank=True)
