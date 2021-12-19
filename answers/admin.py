bf8c022ea25721831c7714015fa396ce8fec86a7
albert3rd <atpcodeio@gmail.com>
2021-12-18 22:48:28 -1000

    Added Models

--- answers/admin.py
+++ answers/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Answers
+from .models import Answers, Trivia_Questions
 # Register your models here.
 admin.site.register(Answers)
+admin.site.register(Trivia_Questions)
