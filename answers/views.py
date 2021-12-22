ae2ce9a9f925534f35a8a57779b6d20b2c858aa9
albert3rd <atpcodeio@gmail.com>
2021-12-21 15:10:52 -1000

    CreateViews

--- answers/views.py
+++ answers/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from answers.models import Answers
 # Create your views here.
+
+class AnswersView(CreateView):
+    model = Answers
+    fields = ['user_answer']
+    template = 'home.html'
+    success_url = '/'
