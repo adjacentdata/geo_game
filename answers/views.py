26012e57a328a55c6024319716c1e8aae10747a7
albert3rd <atpcodeio@gmail.com>
2021-12-23 06:49:19 -1000

    Added HTTP Response

--- answers/views.py
+++ answers/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.http import HttpResponse
 from answers.models import Answers
 # Create your views here.
 
@@ -8,3 +9,7 @@ class AnswersView(CreateView):
     fields = ['user_latitude', 'user_longitude']
     template_name = 'answers/index.html'
     success_url = '/'
+
+def update_score (request):
+    if request.method == 'POST':
+        pass
