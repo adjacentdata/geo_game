56ad99b4d13018f2ac4163eada13a5a77590413e
albert3rd <atpcodeio@gmail.com>
2021-12-23 08:56:50 -1000

    added fields

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
+from typing_extensions import Required
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 # Create your models here.
+
+class Account(AbstractBaseUser):
+    user_name = models.CharField(max_length=25, unique=True)
+    first_name = models.CharField(max_length=25)
+    email = models.CharField(max_length=50, unique=True)
+
+    sign_up_date = models.DateTimeField(auto_now_add=True)
+    last_signin = models.DateTimeField(auto_now_add=True)
+    user_admin = models.BooleanField(default=False)
+    user_active = models.BooleanField(default=False)
+    user_superadmin = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = ['user_name', 'first_name', 'email']
+
+    def __str__(self) -> str:
+        return self.email
