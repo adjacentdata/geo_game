29bbe1b339b1ad3d70d01360309e4986a0b6407c
albert3rd <atpcodeio@gmail.com>
2021-12-25 23:05:13 -1000

    Migration Fixes

--- accounts/models.py
+++ accounts/models.py
@@ -1,30 +1,10 @@
-from typing_extensions import Required
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 # Create your models here.
 
-class Account(AbstractBaseUser):
-    user_name = models.CharField(max_length=25, unique=True)
-    first_name = models.CharField(max_length=25)
-    email = models.CharField(max_length=50, unique=True)
-
-    sign_up_date = models.DateTimeField(auto_now_add=True)
-    last_signin = models.DateTimeField(auto_now_add=True)
-    user_admin = models.BooleanField(default=False)
-    user_active = models.BooleanField(default=False)
-    user_superadmin = models.BooleanField(default=False)
-
-    REQUIRED_FIELDS = ['user_name', 'first_name', 'email']
-
-    def __str__(self):
-        return self.email
-
-    def has_perm (self, perm):
-        return self.user_admin
-
 class AccountManager(BaseUserManager):
-    def create_player(self, first_name, username, email,password=None):
+    def create_user(self, first_name, username, email,password=None):
         if not email:
             raise ValueError("User must have an email address")
         if not username:
@@ -38,3 +18,23 @@ class AccountManager(BaseUserManager):
         player.set_password(password)
         player.save(using=self._db)
         return player
+
+class Account(AbstractBaseUser):
+    username = models.CharField(max_length=25, unique=True)
+    first_name = models.CharField(max_length=25)
+    email = models.CharField(max_length=50, unique=True)
+
+    sign_up_date = models.DateTimeField(auto_now_add=True)
+    last_signin = models.DateTimeField(auto_now_add=True)
+    user_admin = models.BooleanField(default=False)
+    user_active = models.BooleanField(default=False)
+    user_superadmin = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = ['first_name', 'email']
+    USERNAME_FIELD = 'username'
+
+    def __str__(self):
+        return self.email
+
+    def has_perm (self, perm):
+        return self.user_admin
