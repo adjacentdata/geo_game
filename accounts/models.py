6d883baff2c4fe9a65b7c032bbd2698da6e2e99b
albert3rd <atpcodeio@gmail.com>
2022-01-03 13:12:32 -1000

    Project Updates

--- accounts/models.py
+++ accounts/models.py
@@ -19,6 +19,21 @@ class AccountManager(BaseUserManager):
         player.save(using=self._db)
         return player
 
+    def create_superuser(self, first_name, last_name, email, username, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username=username,
+            password=password,
+            first_name=first_name
+        )
+
+        user.is_admin = True
+        user.is_superadmin = True
+        user.is_staff=True
+        user.is_active=True
+        user.save(using=self._db)
+        return user
+
 class Account(AbstractBaseUser):
     username = models.CharField(max_length=25, unique=True)
     first_name = models.CharField(max_length=25)
@@ -26,15 +41,21 @@ class Account(AbstractBaseUser):
 
     sign_up_date = models.DateTimeField(auto_now_add=True)
     last_signin = models.DateTimeField(auto_now_add=True)
-    user_admin = models.BooleanField(default=False)
-    user_active = models.BooleanField(default=False)
-    user_superadmin = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
 
     REQUIRED_FIELDS = ['first_name', 'email']
     USERNAME_FIELD = 'username'
+
+    objects = AccountManager()
 
     def __str__(self):
         return self.email
 
     def has_perm (self, perm):
-        return self.user_admin
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
