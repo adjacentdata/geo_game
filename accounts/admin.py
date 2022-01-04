6ae640b3fc0f94a3b08d2adf9733dbecfd9cf46d
albert3rd <atpcodeio@gmail.com>
2022-01-03 18:32:05 -1000

    passwords edit

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,10 @@ from .models import Account
 from django.contrib.auth.admin import UserAdmin
 
 class AccountAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'username', 'last_login', 'is_active')
+    list_display = ('email', 'first_name', 'username', 'last_login', 'is_active', 'sign_up_date', 'last_signin')
+    filter_horizontal = ()
+    list_filter=()
+    fieldsets =()
 
 # Register your models here.
-admin.site.register(Account)
+admin.site.register(Account, AccountAdmin)
