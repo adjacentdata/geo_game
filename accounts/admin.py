b884460a68f0f2c3f0dd5f5b2826612375c208a5
albert3rd <atpcodeio@gmail.com>
2022-01-03 18:42:55 -1000

    New Listings

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,10 @@ from .models import Account
 from django.contrib.auth.admin import UserAdmin
 
 class AccountAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'username', 'last_login', 'is_active', 'sign_up_date', 'last_signin')
+    list_display = ('email', 'first_name', 'username', 'is_active', 'sign_up_date', 'last_signin')
+    list_display_links = ('email', 'first_name')
+    readonly_fields = ('last_signin', 'sign_up_date')
+    ordering = ('sign_up_date', )
     filter_horizontal = ()
     list_filter=()
     fieldsets =()
