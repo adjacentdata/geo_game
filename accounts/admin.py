7b11a507fda826e90b1ed08ca084c6e6dddcad6c
albert3rd <atpcodeio@gmail.com>
2022-01-03 16:46:53 -1000

    Changing display of accounts admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Account
+from django.contrib.auth.admin import UserAdmin
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'username', 'last_login', 'is_active')
 
 # Register your models here.
 admin.site.register(Account)
